e17a29dcc0b4b5e94831e29a6c459d093eb813e5
Fredokun <fredokun@github.com>
2016-08-11 17:15:06 +0200

    version string

---
+++ mrpython/version.py
@@ -0,0 +1,13 @@
+
+MRPYTHON_VERSION_MAJOR = 0
+MRPYTHON_VERSION_MINOR = 1
+MRPYTHON_VERSION_PATCH = 3
+MRPYTHON_VERSION_TAG = ""
+
+
+def version_string():
+    return "{}.{}.{}{}".format(MRPYTHON_VERSION_MAJOR,
+                               MRPYTHON_VERSION_MINOR,
+                               MRPYTHON_VERSION_PATCH,
+                               "" if MRPYTHON_VERSION_TAG == "" else ("-" + MRPYTHON_VERSION_TAG))
+
