007d8244f08c78109c86e83726243b619d4e290c
Fredokun <fredokun@github.com>
2016-09-26 11:09:40 +0200

    more tests

---
+++ tests/fact.py
@@ -0,0 +1,21 @@
+
+def fact(n):
+    """doc"""
+    
+    # r : Number
+    r = 1
+
+    # i : int
+    i = 2
+
+    while i <= n:
+        r = r * i
+        i = i + 1
+        print("i="+str(i))
+
+    return r
+
+fact(5)
+
+
+
