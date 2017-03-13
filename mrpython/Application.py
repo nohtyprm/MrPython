8ccf19b16e70ad3df1687558dcec775e2929e6c6
GitAdrien <adrien.miss@gmail.com>
2017-03-13 16:50:13 +0100

    issues 24 complete

--- mrpython/Application.py
+++ mrpython/Application.py
@@ -148,20 +148,20 @@ class Application:
     def new_file(self, event=None):
         """ Creates a new empty editor and put it into the pyEditorList """
         file_editor = PyEditor(self.editor_list)
-        self.editor_list.add(file_editor, text=file_editor.get_file_name())
+        self.editor_list.add(file_editor, self.main_view.editor_widget, text=file_editor.get_file_name())
 
     def open(self, event=None):
         """ Open a file in the text editor """
         file_editor = PyEditor(self.editor_list, open=True)
         if (self.editor_list.focusOn(file_editor.long_title()) == False):
             if (file_editor.isOpen()):
-                self.editor_list.add(file_editor, text=file_editor.get_file_name())
+                self.editor_list.add(file_editor, self.main_view.editor_widget, file_editor.get_file_name())
 
 
     def close_all_event(self, event=None):
         """ Quit all the PyEditor : called when exiting application """
         while self.editor_list.get_size() > 0:
-            reply = self.editor_list.close_current_editor()
+            reply = self.editor_list.close_current_editor(self.main_view.editor_widget)
             if reply == "cancel":
                 break
         if self.editor_list.get_size() == 0:
