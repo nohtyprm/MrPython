f89b11335febc20c6fa3ecabeb8c170cb79d63a7
fredokun <frederic.peschanski@lip6.fr>
2017-08-29 14:27:07 +0200

    asynchronous com

--- mrpython/mp/MPClient.py
+++ mrpython/mp/MPClient.py
@@ -5,13 +5,10 @@ import time
 
 import MPServer
 
-
-
-
 if __name__ == "__main__":
     #mp.set_start_method('fork')
     
-    print("Start client")
+    print("[Client] start")
 
     request, reply = mp.Pipe()
     serv = mp.Process(target=MPServer.run_server, args=(reply,))
@@ -21,8 +18,18 @@ if __name__ == "__main__":
     #time.sleep(3)
     print("[Client] send 'test'")
     request.send("test")
-    
+
+    print("[Client] wait loop")
+    avail = False
+    while not avail:
+        print("[Client] wait 1 second")
+        avail = request.poll(1)
+
+    msg = request.recv()
+    print("[Client] receives '{}'".format(msg))
+    print("[Client] terminates server")
+    serv.terminate() 
     serv.join()
-    print("Stop client")
+    print("[Client] stop")
 
     
