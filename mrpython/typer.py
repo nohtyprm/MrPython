08f1c1ba5b87026b85b55b6ee071ffa1037ba864
Alexandre Doussot <doussot.alexandre@gmail.com>
2018-02-20 12:32:25 +0000

    Refactor typer

--- mrpython/typer.py
+++ mrpython/typer.py
@@ -20,26 +20,27 @@ class Type():
             res += ", " + str(self.lineno)
         return res + ")"
 
-def get_annotations(filename):
-    ''' string -> (id -> Type)
-        Returns a dictionary of type information from a
-        file name.
+# FIXME: When we'll call this functions, we'll probably have an offset problem
+def get_annotations_from_ast(tree, codelines):
+    ''' ast -> string list -> Type dict)
+        Returns a dictionary of type information from an
+        ast and a list of lines of code. The latter is needed
+        in order to get back the annotation.
     '''
     type_dict = {}
-    with open(filename) as file:
-        # Build a list of all lines for easy access to specific line
-        code = file.readlines()
-        # Get back to start of file for ast parsing
-        file.seek(0)
-
-        walker = AssignAggregator()
-        parser = TypeAnnotationParser()
-        walker.visit(ast.parse(file.read()))
+    walker = AssignAggregator()
+    parser = TypeAnnotationParser()
+    walker.visit(tree)
 
     for assign in walker.assigns:
-        parse_result = parser.parse_from_string(code[assign.lineno - 2])
+        parse_result = parser.parse_from_string(codelines[assign.lineno - 2])
         if not parse_result.iserror:
             var_type = Type(parse_result.content.type, assign.lineno)
             type_dict[parse_result.content.name] = var_type
 
     return type_dict
+
+def get_annotations(code_str):
+    ''' String -> Type Dict '''
+    codelines = code_str.readlines()
+    return get_annotations_from_ast(ast.parse(code_str), codelines)
