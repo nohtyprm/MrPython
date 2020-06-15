f212e5092c32d73ee23df6ba8b8aa01501b9bda9
Adil <adil-75@live.fr>
2020-06-15 12:06:59 +0200

    lrs properties

--- mrpython/tincan/lrs_properties.py
+++ mrpython/tincan/lrs_properties.py
@@ -1,7 +1,7 @@
 """
 Contains user-specific information for testing.
 """
-
+"""
 endpoint="https://cloud.scorm.com/lrs/90J41PMBAW/"
 version ="1.0.1" # 1.0.1 | 1.0.0 | 0.95 | 0.9
 username="gIX4lEOMvbXHPCLNmbc"
@@ -9,6 +9,5 @@ password="DKXdFLk8RwgUXsvg2Xk"
 """
 endpoint="https://lrsmocah.lip6.fr/data/xAPI"
 version ="1.0.1" # 1.0.1 | 1.0.0 | 0.95 | 0.9
-username="d7651359375594797bb009f877c655cf1334115c"
-password="339818e63152ff7dafcb4da95e40cd33a6f9130b"
-"""
+username="8cba24cdc8d3306dd8cd917d586fb5972789efbc"
+password="3c480cc7dac4b8556120a9f37467371f495d8210"
