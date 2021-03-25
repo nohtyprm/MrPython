65b159e0b3e8677b10372f76fbec3b0508bafa4e
azg <azzougkouceila1998@gmail.com>
2021-03-25 12:41:11 +0100

    tests double declaration were added

---
+++ test/progs/double_declaration_KO.py
@@ -0,0 +1,9 @@
+
+def f(a:int,b:int)->int:
+    """ Retourne la somme de a et b"""
+    x:int
+    x:int
+
+    return a+b
+
+print(f(2,3))
---
+++ test/progs/double_declaration_function_KO.py
@@ -0,0 +1,17 @@
+
+def f(a:int,b:int)->int:
+    """ Retourne la somme de a et b"""
+    #x:int
+    #x:int
+
+    return a+b
+
+def f(a:int,b:int)->int:
+    """ Retourne la soustraction de a et b"""
+    #x:int
+    #x:int
+
+    return a-b
+
+
+assert f(2,3)==5
