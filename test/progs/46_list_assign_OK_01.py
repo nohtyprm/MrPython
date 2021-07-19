de4e949f1307a46c4bf996276a31dee2ac7aa198
Fredokun <frederic.peschanski@lip6.fr>
2021-07-19 17:56:58 +0200

    list assignment test

---
+++ test/progs/46_list_assign_OK_01.py
@@ -0,0 +1,16 @@
+
+def incrementer(l : List[int]) -> None:
+    """Incrémente tous les éléments de la liste l.
+    """
+    i : int
+    for i in range(len(l)):
+        l[i] = l[i] + 1
+
+# Jeu de tests
+
+MaListe : List[int] = [1, 2, 3, 4, 5, 6]
+
+assert incrementer(MaListe) == None
+assert MaListe == [2, 3, 4, 5, 6, 7]
+
+
