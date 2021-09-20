e9ca7831516179b31f8f0d9202e42b68f75848c8
Fredokun <frederic.peschanski@lip6.fr>
2021-09-20 15:45:33 +0200

    quick fix for py37 (precondition parsing)

--- mrpython/StudentRunner.py
+++ mrpython/StudentRunner.py
@@ -319,7 +319,10 @@ class FunctionDefVisitor(ast.NodeTransformer):
                 assert_node = ast.Assert(precondition_node)
                 assert_node.lineno = lineno
                 ast_asserts.append(assert_node)
-            node_res = ast.FunctionDef(node.name,node.args,ast_asserts+node.body,node.decorator_list,node.returns,node.type_comment,lineno = node.lineno,col_offset = node.col_offset, end_lineno = node.lineno, end_col_offset = node.end_col_offset)
+            if hasattr(node, "type_comment"):
+                node_res = ast.FunctionDef(node.name,node.args,ast_asserts+node.body,node.decorator_list,node.returns,node.type_comment,lineno = node.lineno,col_offset = node.col_offset, end_lineno = node.lineno, end_col_offset = node.end_col_offset)
+            else: # python 3.7
+                node_res = ast.FunctionDef(node.name,node.args,ast_asserts+node.body,node.decorator_list,node.returns,lineno = node.lineno,col_offset = node.col_offset, end_lineno = node.lineno)
             return node_res
 
         
